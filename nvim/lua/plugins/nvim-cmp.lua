5d7152546d802e333b0640b559fd731d4d19c924
c0per <60544162+c0per@users.noreply.github.com>
2024-11-15 15:55:15 +0800

    update

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,34 +1,56 @@
 return {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
 
-        -- vsnip
-        'hrsh7th/cmp-vsnip',
-        'hrsh7th/vim-vsnip',
-    },
+            -- LuaSnip
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip'
+        },
 
-    config = function ()
-        local cmp = require 'cmp'
+        config = function ()
+            local cmp = require 'cmp'
 
-        cmp.setup {
-            snippet = { expand = function (args) vim.fn["vsnip#anonymous"](args.body) end },
-            mapping = {
-                ['<C-y>'] = cmp.mapping.confirm { select = true, behavior = cmp.ConfirmBehavior.Replace },
-                ['<C-n>'] = cmp.mapping.select_next_item(),
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4)
-            },
-            sources = cmp.config.sources {
-                { name = 'nvim_lsp' },
-                { name = 'buffer' },
-                { name = 'path' },
-                { name = 'cmdline' },
-                { name = 'vsnip' }
+            cmp.setup {
+                snippet = { expand = function (args) require 'luasnip'.lsp_expand(args.body) end },
+                mapping = {
+                    ['<C-y>'] = cmp.mapping.confirm { select = true, behavior = cmp.ConfirmBehavior.Replace },
+                    ['<C-n>'] = cmp.mapping.select_next_item(),
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4)
+                },
+                sources = cmp.config.sources {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                }
             }
-        }
-    end
+
+            cmp.setup.cmdline({'/', '?'}, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                  { name = 'buffer' }
+                }
+            })
+
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources(
+                    { { name = 'path' } },
+                    { { name = 'cmdline' } }
+                ),
+                matching = { disallow_symbol_nonprefix_matching = false }
+            })        
+        end
+    },
+    {
+        'L3MON4D3/LuaSnip',
+        version = "v2.*",
+	build = "make install_jsregexp"
+    }
 }
