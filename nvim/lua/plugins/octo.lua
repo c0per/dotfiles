8c468729916c121300c7afed1be103797f1765e2
c0per <60544162+c0per@users.noreply.github.com>
2024-12-17 12:18:08 +0800

    feat: change some keybindings in octo.nvim

--- nvim/lua/plugins/octo.lua
+++ nvim/lua/plugins/octo.lua
@@ -6,6 +6,14 @@ return {
         'nvim-tree/nvim-web-devicons',
     },
     config = function()
-        require 'octo'.setup {}
+        require 'octo'.setup {
+            mappings = {
+                review_diff = {
+                    add_review_comment = { lhs = "<leader>ca", desc = "add a new review comment" },
+                    add_review_suggestion = { lhs = "<leader>sa", desc = "add a new review suggestion" },
+                    toggle_viewed = { lhs = "<leader><space>", desc = "toggle viewer viewed state" },
+                }
+            }
+        }
     end
 }
