787dc1e7a5a9c5826aeec08894a242089d08ca8a
c0per <60544162+c0per@users.noreply.github.com>
2025-04-15 11:13:05 +0800

    fix nvim biome

--- nvim/lsp/biome.lua
+++ nvim/lsp/biome.lua
@@ -1,5 +1,16 @@
 return {
     cmd = { 'biome', 'lsp-proxy' },
     filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'json', 'json5', 'jsonc' },
-    root_markers = { 'biome.json' }
+
+    root_dir = function(bufnr, cb)
+        local filename = vim.api.nvim_buf_get_name(bufnr)
+
+        local config_file = vim.fs.find({ 'biome.json' }, {
+            path = vim.fs.dirname(filename),
+            upward = true,
+        })[1]
+
+        local root = config_file and vim.fs.dirname(config_file) or nil
+        cb(root)
+    end
 }
--- nvim/lsp/tailwindcss.lua
+++ nvim/lsp/tailwindcss.lua
@@ -1,4 +1,25 @@
 return {
     cmd = { 'tailwindcss-language-server', '--stdio' },
-    filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
+    filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+    settings = {
+      tailwindCSS = {
+        validate = true,
+        lint = {
+          cssConflict = 'warning',
+          invalidApply = 'error',
+          invalidScreen = 'error',
+          invalidVariant = 'error',
+          invalidConfigPath = 'error',
+          invalidTailwindDirective = 'error',
+          recommendedVariantOrder = 'warning',
+        },
+        classAttributes = { 'class', 'className' },
+        includeLanguages = {
+          eelixir = 'html-eex',
+          eruby = 'erb',
+          templ = 'html',
+          htmlangular = 'html',
+        },
+    }
+  }
 }
